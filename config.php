934fbda541db8b5a853b6e992e97604007e917ea
Shaun Daubney <superawesomeme@googlemail.com>
2012-05-22 11:31:57 +0200

    Update config.php

--- config.php
+++ config.php
@@ -47,7 +47,7 @@ $THEME->layouts = array(
         'regions' => array('side-pre', 'side-post'),
         'defaultregion' => 'side-pre',
     ),
-    'general' => array(
+    'standard' => array(
         'file' => 'general.php',
         'regions' => array('side-pre', 'side-post'),
         'defaultregion' => 'side-pre',
