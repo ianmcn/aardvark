c8fe8ca12b3ad7a69d3f24d76e5b979aed3098cf
ianmcn <i.mcnaught@gmail.com>
2012-10-20 11:15:05 +0400

    Update layout/general.php
    added the hack to make placeholder text work in IE (ref: http://stackoverflow.com/questions/8628522/placeholder-not-working-for-internet-explorer)

--- layout/general.php
+++ layout/general.php
@@ -44,7 +44,38 @@ echo $OUTPUT->doctype() ?>
     <title><?php echo $PAGE->title ?></title>
     <link rel="shortcut icon" href="<?php echo $OUTPUT->pix_url('favicon', 'theme')?>" />
     <?php echo $OUTPUT->standard_head_html() ?>
-    
+<script type="text/javascript">
+window.onload = function() {
+    var arrInputs = document.getElementsByTagName("input");
+    for (var i = 0; i < arrInputs.length; i++) {
+        var curInput = arrInputs[i];
+        if (!curInput.type || curInput.type == "" || curInput.type == "text")
+            HandlePlaceholder(curInput);
+    }
+};
+
+function HandlePlaceholder(oTextbox) {
+    if (typeof oTextbox.placeholder == "undefined") {
+        var curPlaceholder = oTextbox.getAttribute("placeholder");
+        if (curPlaceholder && curPlaceholder.length > 0) {
+            oTextbox.value = curPlaceholder;
+            oTextbox.setAttribute("old_color", oTextbox.style.color);
+            oTextbox.style.color = "#c0c0c0";
+            oTextbox.onfocus = function() {
+                this.style.color = this.getAttribute("old_color");
+                if (this.value === curPlaceholder)
+                    this.value = "";
+            };
+            oTextbox.onblur = function() {
+                if (this.value === "") {
+                    this.style.color = "#c0c0c0";
+                    this.value = curPlaceholder;
+                }
+            };
+        }
+    }
+}
+</script>
 </head>
 <body id="<?php p($PAGE->bodyid) ?>" class="<?php p($PAGE->bodyclasses.' '.join(' ', $bodyclasses)) ?>">
 <?php echo $OUTPUT->standard_top_of_body_html() ?>
